627caaa4d7c00392d0fb42942083edfe165d4855
Sergio Martin-Albo Calvera <sergiomartinmir@gmail.com>
2021-11-14 20:49:43 +0100

    Añadidos primeros Modelos y Admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Songs, Artist, Album
 # Register your models here.
+
+@admin.register(Songs)
+class SongsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'artist', 'album', 'release_date', 'views']
+    list_filter = ['title', 'album','release_date', 'views']
+
+
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ['name', 'debutYear', 'biography', 'image']
+    list_filter = ['name', 'debutYear']
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    pass
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,53 @@
+from django.core.files.images import ImageFile
 from django.db import models
+from django.db.models.fields import CharField, DateField, IntegerField, TextField
+from django.db.models.fields.files import ImageFieldFile
 
 # Create your models here.
+
+
+class Artist(models.Model):
+    """Modelo para los artistas/Bandas"""
+
+    name = CharField("Nombre Grupo/Cantante", max_length=80)
+    debutYear = DateField("Año de debut")
+    biography = TextField("Biografia")
+    image = ImageFile("Imagen")
+    #country
+    #label
+
+    def __str__(self) :
+        return self.name
+
+
+class Album(models.Model):
+    """Modelo de Album"""
+
+    title = CharField("Titulo", max_length=80)
+    artist = models.ForeignKey(Artist, on_delete=models.SET_NULL, blank=True) 
+    releaseDate = DateField("Año de debut")
+    #image = ImageFile
+    
+    def __str__(self) :
+        return self.title
+
+
+class Songs(models.Model):
+
+    """Modelo Para Canciones"""
+
+    title = CharField("Titulo",max_length=50)
+    artist = models.ForeignKey(Artist, on_delete=models.SET_NULL, null=True)
+    #collab_artists
+    album =  models.ForeignKey(Album, on_delete=models.SET_NULL, null=True)
+    release_date = DateField("Fecha de lanzamiento")
+    views = IntegerField("Visitas", blank=True)
+    #genre =
+    #language
+
+    def __str__(self) :
+        return self.title
+    
+    class Meta:
+        verbose_name='Song'
+        verbose_name_plural='Songs'
